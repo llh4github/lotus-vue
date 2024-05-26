6ed1dfdbc5ac011e57814ae8f8bd3d86619e0c33
llh <lilinhong_coding@foxmail.com>
2024-05-26 20:46:43 +0800

    🔧: 修改配置

--- eslint.config.js
+++ eslint.config.js
@@ -104,7 +104,7 @@ export default defineFlatConfig([
         { allowBitwiseExpressions: true }
       ],
       "@typescript-eslint/no-unused-vars": [
-        "error",
+        "warn",
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_"
